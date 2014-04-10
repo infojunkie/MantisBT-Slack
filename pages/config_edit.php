bb2b1470574b1cb395fb8c24380f1c1941516b34
infojunkie <karim.ratib@gmail.com>
2014-04-09 20:39:02 -0700

    Add GPL header

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Slack Integration
+ * Copyright (C) 2014 Karim Ratib (karim.ratib@gmail.com)
+ *
+ * Slack Integration is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * Slack Integration is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Slack Integration; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * or see http://www.gnu.org/licenses/.
+ */
+
 form_security_validate( 'plugin_Slack_config_edit' );
 
 auth_reauthenticate( );
